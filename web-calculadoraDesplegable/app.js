41969eda1eb8472e6b639265ade47b4aca11ed34
Teo <teo.lairla@iessierradeguara.com>
2020-10-28 18:18:16 +0100

    Seguircalculadora

--- web-calculadoraDesplegable/app.js
+++ web-calculadoraDesplegable/app.js
@@ -1,7 +1,20 @@
 function calcular() {
     let op1 = document.getElementById("op1");
     let op2 = document.getElementById("op2");
+    let operacion = document.getElementById("operacion");
+    console.log (operacion);
+    let resultado;
+    let resultado = resta(op1.value, op2.value);
     let resultado = suma(op1.value, op2.value);
+
+    if (operacion.value == "suma"){
+        resultado = suma(op1.value, op2.value);
+    }else if (operacion.value == "resta"){
+        resultado = resta(op1.value, op2.value);
+    }else if (operacion.value == "multiplicacion"){
+        resultado = multiplicacion(op1.value, op2.value);
+    }
+
 
     if (resultado != null) {
         document.getElementById("res").innerHTML= resultado;    
@@ -13,6 +26,28 @@ function calcular() {
 
 
 function suma(o1, o2) {
+    let resultado=0;
+    
+     if(isNaN(o1) == true ||  isNaN(o2) == true ){
+        resultado = null;
+     }else{
+        resultado = (parseFloat(o1) +  parseFloat(o2)).toFixed(2);
+     }
+    return resultado;
+}
+
+function resta(o1, o2) {
+    let resultado=0;
+    
+     if(isNaN(o1) == true ||  isNaN(o2) == true ){
+        resultado = null;
+     }else{
+        resultado = (parseFloat(o1) +  parseFloat(o2)).toFixed(2);
+     }
+    return resultado;
+}
+
+function multiplicacion(o1, o2) {
     let resultado=0;
     
      if(isNaN(o1) == true ||  isNaN(o2) == true ){
