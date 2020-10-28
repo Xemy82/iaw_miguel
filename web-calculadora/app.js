81deb6ae653217fedbdf374331ac6431507206e7
Teo <teo.lairla@iessierradeguara.com>
2020-10-28 17:27:46 +0100

    Calculadora

--- web-calculadora/app.js
+++ web-calculadora/app.js
@@ -1,31 +1,28 @@
-function imprimirsuma() {
-    let op1 = document.getElementById("op1")
-    let op2 = document.getElementById("op2")
+function imprimirSuma() {
+    let op1 = document.getElementById("op1");
+    let op2 = document.getElementById("op2");
     let resultado = suma(op1.value, op2.value);
-    /*
-    TODO rellena el párrafo con id resultado sólo si el resultado
-    no es null
-    */ 
-    if (resultado != null){
-    document.getElementById("res").innerHTML = resultado;
-    }else{
 
-    }
+    /*
+    TODO rellenar el párrafo con id resultado sólo si el resultado no es null 
+    */
+
+    
+    document.getElementById("res").innerHTML= resultado;
+    
+}
+
 
 function suma(o1, o2) {
-    let resultado=0;
+    let resultado;
+    /* TODO: 
+     - Comprobar que los parámetros son números
+     - Si los dos son números sumarlos en la variable resultado
+     - Si alguno de los dos no es número resultado = null
+     
+     */
 
-    /* TODO:
-    - Comprobar que los parametros son números 
-    - Si los dos son números, sumarlos en resultado.
-    isNAN() devuelve cierto si el valor de a, no es un número.
-    - Si alguno de los dos no es número resultado = null
-    */
-     if (isNAN(o1) == true || isNAN(o2) == true){
-         resultado = null;    
-     }else{
-         resultado = parseFloat(o1)+parseFloat(o2);
-         //resultado = "o1" + "o2";
-     }
+
+
     return resultado;
 }
