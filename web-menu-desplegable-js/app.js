430c51257d1f625e460419b78f8840162481fb5a
Miguel <115831@iessierradeguara.com>
2020-11-03 10:06:18 +0100

    Analizando sintaxis

--- web-menu-desplegable-js/app.js
+++ web-menu-desplegable-js/app.js
@@ -14,9 +14,9 @@ function mostrarMenu() {
     let menu = document.getElementById("listaMenu");
     console.log(menu);
     menu.children[0].children[1].style.display = "inline";
-    menu.children[1].children[1].style.display = "inline";
+    menu.children[1].children[3].style.display = "inline";
     menu.children[2].children[1].style.display = "inline";
-    menu.children[3].children[1].style.display = "inline";
+    menu.children[3].children[2].style.display = "inline";
 
 
     /* ---- */
@@ -26,10 +26,11 @@ function mostrarMenu() {
 function ocultarMenu() {
     /* codigo para ocultar el menu */
     let menu = document.getElementById("listaMenu");
+    console.log(menu);
     menu.children[0].children[1].style.display = "none";
-    menu.children[1].children[1].style.display = "none";
+    menu.children[1].children[3].style.display = "none";
     menu.children[2].children[1].style.display = "none";
-    menu.children[3].children[1].style.display = "none";
+    menu.children[3].children[2].style.display = "none";
 
 
 
