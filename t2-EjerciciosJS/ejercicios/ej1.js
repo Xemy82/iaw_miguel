3824ce3c87267b9ae2107e2349e3d6a0d5ea2f15
Miguel <115831@iessierradeguara.com>
2020-11-16 07:25:57 +0100

    t2-jmiguel

---
+++ t2-EjerciciosJS/ejercicios/ej1.js
@@ -0,0 +1,24 @@
+// valor 1
+alert("Empieza el ejercicio 1");
+// Crea 2 variables numéricas
+var num1 = 5;
+var num2 = 4;
+
+// Imprime un alert con la suma de ambas.
+
+alert(num1 + num2);
+
+// Imprime un alert con la resta de ambas.
+
+alert(num1 - num2);
+
+// Imprime un alert con el producto de ambas.
+
+alert(num1 * num2);
+
+// Cambia el valor de la segunda variable a 0.
+
+alert(num1 / num2);
+
+// Imprime la division de ambas.
+
