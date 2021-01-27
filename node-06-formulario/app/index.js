4ceaaf7488a50534d5b07e72c7bb3453761a55b2
Miguel <115831@iessierradeguara.com>
2021-01-27 18:05:56 +0100

    node-06

--- node-06-formulario/app/index.js
+++ node-06-formulario/app/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 app.set('puerto',3000);
+
+//con body-parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false}))
+
+
+
 // todo usar body Parser y sanitizar datos.
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname,"form.html"));
@@ -10,6 +16,23 @@ app.get('/', function (req, res) {
 
 app.post('/form', function (req,res) {
   //todo capturar datos
+  console.log("petición recibida:"+req.method);
+/*
+//sin body-parser
+  let body = '';
+  req.on('data', function (dato){
+    body+= dato.toString();
+  });
+  req.on('end',function(){
+    console.log(body);
+    res.sendFile(path.join(__diname,"form.html"));
+  })
+*/
+//con body-parser
+let recibido = req.body;
+console.log(recibido);
+res.sendStatus(200);
+
 })
 
 
