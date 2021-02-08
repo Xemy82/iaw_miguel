fad197dbfb0fc93e296e60392c4c9c994ebaeea0
Miguel <115831@iessierradeguara.com>
2021-02-08 20:13:32 +0100

    node-10

--- node-10-ejercicio/cliente-fetch/app.js
+++ node-10-ejercicio/cliente-fetch/app.js
@@ -1,14 +1,30 @@
 alert('Aun no hemos empezado');
 
-fetch('http://localhost:3000')
+fetch('http://localhost:3000/lista')
     .then( respuesta => respuesta.json()) //convertir esto a json
-    .then( objeto => rellenarParrafo(objeto.nombre) )
+    .then( objeto => rellenarLista(objeto.lista) )
+    .catch( error => console.error( 'Fallo:', error));
+
+fetch('http://localhost:3000/texto')
+    .then( respuesta => respuesta.json()) //convertir esto a json
+    .then( objeto => rellenarTexto(objeto.texto) )
     .catch( error => console.error( 'Fallo:', error));
 
 alert('Ya hemos empezado');
 
-function rellenarParrafo(objeto){
+function rellenarLista(lista){
     //let p = document.getElementById('idPar');
-    let p = document.querySelector('#idPar')
-    p.innerHTML = texto;
+    let elUl = document.createElement('ul');
+    document.body.appendChild(elUl)
+    for(const texto of lista) {
+        let elLi = document.createElement('li');
+        elLi.innerHTML = texto;
+        elUl.appendChild(elLi);
+    }
+}
+
+function rellenarTexto(txt) {
+    let elementoP = document.createElement('p');
+    elementoP.innerHTML = txt;
+    document.body.appendChild(elementoP);
 }
--- node-10-ejercicio/servidor/index.js
+++ node-10-ejercicio/servidor/index.js
@@ -2,10 +2,20 @@ const express = require('express');
 const app = express();
 const puerto = 3000;
 
-app.get('/', function (req,res) {
+app.get('/lista', function (req,res) {
     let respuesta = {
         //"nombre": "Alumno"
-        "ul": ["hola", "adios"]
+        "lista": ["hola", "adios"]
+    }
+    // Permitir conexiones desde otros origenes.
+    res.header("Access-Control-Allow-Origin", "*");
+    res.json(respuesta);
+})
+
+app.get('/texto', function (req,res) {
+    let respuesta = {
+        //"nombre": "Alumno"
+        "texto": "Hola mundo"
     }
     // Permitir conexiones desde otros origenes.
     res.header("Access-Control-Allow-Origin", "*");
